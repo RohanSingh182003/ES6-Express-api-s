d411a2407c22d51acca9087d76ef188c96cfb8ba
Rohan SIngh <rs7289579@gmail.com>
2023-01-24 21:28:18 +0530

    make crud operations functional and encrypt password

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,19 +1,63 @@
-export const getUsers = (req,res) => {
-    res.send('users')
-}
+import CryptoJS from "crypto-js";
+import User from "../models/users.js";
+import { validationResult } from "express-validator";
 
-export const getUser = (req,res) => {
-    res.send('user')
-}
+const PAS_SECRET = process.env.PAS_SECRET || "SuperStrongSecretKey"; // development purpose only, not included in production
 
-export const loginUser = (req,res) => {
-    res.send('login')
-}
+export const getUsers = async (req, res) => {
+  try {
+    const response = await User.find()
+    res.send(response)
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+};
 
-export const registerUser = (req,res) => {
-    res.send('register')
-}
+export const getUser = async (req, res) => {
+    try {
+        const response = await User.findById(req.params.id)
+        res.send(response)
+      } catch (err) {
+        res.status(500).send(err.message)
+      }
+};
 
-export const updateUser = (req,res) => {
-    res.send('update')
+export const loginUser = (req, res) => {
+  res.send("login");
+};
+
+export const registerUser = async (req, res) => {
+  try {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { password, confirmPassword, ...others } = req.body;
+    if (password != confirmPassword)
+      return res.status(400).send("passwords doesn't matched");
+    const enc_password = CryptoJS.AES.encrypt(password, PAS_SECRET).toString(); // Encrypt password
+    const response = await User.create({ ...others, password: enc_password });
+    res.status(201).send(response);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const response = await User.findByIdAndUpdate(req.params.id,{ $set : req.body},{new : true})
+    res.send(response)
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
+};
+
+export const deleteUser = async (req,res) => {
+  try {
+    const response = await User.findByIdAndDelete(req.params.id)
+    res.send(response)
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,15 @@
 import express from 'express'
-import { getUser, getUsers, loginUser, registerUser, updateUser } from '../controllers/users.js';
+import { deleteUser, getUser, getUsers, loginUser, registerUser, updateUser } from '../controllers/users.js';
+import { body } from 'express-validator';
+
 
 const router = express.Router()
 
 router.get('/',getUsers)
 router.get('/:id',getUser)
-router.post('/',registerUser)
+router.post('/',[body('email').isEmail(),body('password').isLength({ min: 6 , max: 12 })],registerUser)
 router.post('/login',loginUser) // loginUser
 router.put('/:id',updateUser)
+router.delete('/:id',deleteUser)
 
 export default router;
