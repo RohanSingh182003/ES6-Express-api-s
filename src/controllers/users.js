0c55750e6a3f290fc52b6d489ac09311335b2b7a
Rohan SIngh <rs7289579@gmail.com>
2023-01-25 18:52:33 +0530

    remove password from all api responses

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,29 +1,50 @@
 import CryptoJS from "crypto-js";
 import User from "../models/users.js";
+import jwt from "jsonwebtoken";
 import { validationResult } from "express-validator";
 
-const PAS_SECRET = process.env.PAS_SECRET || "SuperStrongSecretKey"; // development purpose only, not included in production
+// development purpose only, it's not hardcoded in production
+const PAS_SECRET = process.env.PAS_SECRET || "SuperStrongSecretKey";
+const JWT_SEC = process.env.JWT_SEC || "JWTSecretKey";
 
 export const getUsers = async (req, res) => {
   try {
-    const response = await User.find()
-    res.send(response)
+    const response = await User.find({}, { password: 0 }); // this one reture everyfield except password, use 1 to include and use 0 to exclude
+    res.send(response);
   } catch (err) {
-    res.status(500).send(err.message)
+    res.status(500).send(err.message);
   }
 };
 
 export const getUser = async (req, res) => {
-    try {
-        const response = await User.findById(req.params.id)
-        res.send(response)
-      } catch (err) {
-        res.status(500).send(err.message)
-      }
+  try {
+    const response = await User.findById(req.params.id, { password: 0 });
+    res.send(response);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
 
-export const loginUser = (req, res) => {
-  res.send("login");
+export const loginUser = async (req, res) => {
+  try {
+    const email = req.body.email;
+    const currentPassword = req.body.password;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).send("User doesn't exists");
+    //decrypt password
+    const dec_password = CryptoJS.AES.decrypt(
+      user.password,
+      PAS_SECRET
+    ).toString(CryptoJS.enc.Utf8);
+    // validate password
+    if (currentPassword != dec_password)
+      return res.status(401).send("wrong credentials");
+    const { password, ...others } = user._doc;
+    const token = jwt.sign({ others }, JWT_SEC, { expiresIn: "12h" });
+    res.send({ ...others, token });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
 
 export const registerUser = async (req, res) => {
@@ -33,12 +54,17 @@ export const registerUser = async (req, res) => {
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    const { password, confirmPassword, ...others } = req.body;
-    if (password != confirmPassword)
+    let { confirmPassword, ...others } = req.body;
+    let currentPassword = req.body.password;
+    if (currentPassword != confirmPassword)
       return res.status(400).send("passwords doesn't matched");
-    const enc_password = CryptoJS.AES.encrypt(password, PAS_SECRET).toString(); // Encrypt password
+    const enc_password = CryptoJS.AES.encrypt(
+      currentPassword,
+      PAS_SECRET
+    ).toString(); // Encrypt password
     const response = await User.create({ ...others, password: enc_password });
-    res.status(201).send(response);
+    const { password, ...data } = response._doc;
+    res.status(201).send(data);
   } catch (err) {
     res.status(500).send(err.message);
   }
@@ -46,18 +72,26 @@ export const registerUser = async (req, res) => {
 
 export const updateUser = async (req, res) => {
   try {
-    const response = await User.findByIdAndUpdate(req.params.id,{ $set : req.body},{new : true})
-    res.send(response)
+    const response = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    if (!response) return res.status(404).send("user doesn't exixts");
+    const { password, ...others } = response._doc;
+    res.send(others);
   } catch (err) {
-    res.status(500).send(err.message)
+    res.status(500).send(err.message);
   }
 };
 
-export const deleteUser = async (req,res) => {
+export const deleteUser = async (req, res) => {
   try {
-    const response = await User.findByIdAndDelete(req.params.id)
-    res.send(response)
+    const response = await User.findByIdAndDelete(req.params.id);
+    if (!response) return res.status(404).send("user doesn't exixts");
+    const { password, ...others } = response._doc;
+    res.send(others);
   } catch (err) {
-    res.status(500).send(err.message)
+    res.status(500).send(err.message);
   }
-}
+};
